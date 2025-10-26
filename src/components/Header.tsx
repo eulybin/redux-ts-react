f0c4bc8a7f942852a66e7981775e08f1340a1147
Egor Ulybin <egorulybin@gmail.com>
2025-10-26 21:22:45 +0100

    custom useSelector hook and showing cart quantity in header comp

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,34 @@
 import { useState } from 'react';
-
+import { useCartSelector } from '../store/hooks.ts';
 import Cart from './Cart.tsx';
 
 export default function Header() {
-  const [cartIsVisible, setCartIsVisible] = useState(false);
+    const totalCartQuantity = useCartSelector((state) =>
+        state.cart.items.reduce((acc, item) => acc + item.quantity, 0)
+    );
 
-  function handleOpenCartClick() {
-    setCartIsVisible(true);
-  }
+    const [cartIsVisible, setCartIsVisible] = useState(false);
 
-  function handleCloseCartClick() {
-    setCartIsVisible(false);
-  }
+    function handleOpenCartClick() {
+        setCartIsVisible(true);
+    }
 
-  return (
-    <>
-      {cartIsVisible && <Cart onClose={handleCloseCartClick} />}
-      <header id="main-header">
-        <div id="main-title">
-          <img src="logo.png" alt="Elegant model" />
-          <h1>Elegant Redux</h1>
-        </div>
-        <p>
-          <button onClick={handleOpenCartClick}>Cart (0)</button>
-        </p>
-      </header>
-    </>
-  );
+    function handleCloseCartClick() {
+        setCartIsVisible(false);
+    }
+
+    return (
+        <>
+            {cartIsVisible && <Cart onClose={handleCloseCartClick} />}
+            <header id='main-header'>
+                <div id='main-title'>
+                    <img src='logo.png' alt='Elegant model' />
+                    <h1>Elegant Redux</h1>
+                </div>
+                <p>
+                    <button onClick={handleOpenCartClick}>Cart ({totalCartQuantity})</button>
+                </p>
+            </header>
+        </>
+    );
 }
